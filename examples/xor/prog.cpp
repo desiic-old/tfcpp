23ab0f2b6f521319247706a9af30fe3ad5b8d332
Dat Dinhquoc <datdinhquoc@abivin.com>
2019-05-16 21:46:12 +0000

    Msg

---
+++ examples/xor/prog.cpp
@@ -0,0 +1,22 @@
+/*!
+\file
+\brief Test XOR using TFCpp library
+*/
+
+//c++ standard
+#include <iostream>
+
+//custom libs
+#include <tfcpp/t.hpp>
+#include <tfcpp/model.hpp>
+#include <tfcpp/miscs/utils.hpp>
+
+//PROGRAMME ENTRY POINT=========================================================
+/*!
+\brief Main function
+*/
+int main(int Argc,char* Args[]){
+  return 0;
+}
+
+//eof
