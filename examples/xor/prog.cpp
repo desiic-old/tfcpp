6cce8e4ff2fe3c23289d52906aed8aba0e32dda6
Dat Dinhquoc <datdinhquoc@abivin.com>
2019-05-16 23:01:52 +0000

    Msg

--- examples/xor/prog.cpp
+++ examples/xor/prog.cpp
@@ -1,26 +1,51 @@
 /*!
 \file
-\brief Test XOR using TFCpp library
+\brief Test XOR using TFCpp library,
+       NamedType: 
+       https://www.fluentcpp.com/2018/12/14/named-arguments-cpp
+       https://github.com/joboccara/NamedType
 */
 
 //c++ standard
 #include <iostream>
 
 //custom libs
+#include <namedtype/named_type.hpp>
+
 #include <tfcpp/t.hpp>
 #include <tfcpp/model.hpp>
 #include <tfcpp/miscs/utils.hpp>
 
 //namespaces
 using namespace std;
+using namespace fluent;
 using namespace tfcpp;
+
+using text = NamedType<string,struct text_tag>;
+static const text::argument Text;
+
+/*void print(text const& Text){
+  cout <<Text <<endl;
+}*/
+
+using FirstName = NamedType<std::string, struct FirstNameTag>;
+using LastName = NamedType<std::string, struct LastNameTag>;
+
+static const FirstName::argument firstName;
+static const LastName::argument lastName;
+
+void displayName(FirstName const& theFirstName, LastName const& theLastName){
+  cout <<theFirstName.get() <<endl;
+}
+
 
 //PROGRAMME ENTRY POINT=========================================================
 /*!
 \brief Main function
 */
 int main(int Argc,char* Args[]){
-  cout <<"test" <<endl;
+displayName(firstName = "John", lastName = "Doe");
+  //print(Text = "Test named type!");
 }
 
 //eof
