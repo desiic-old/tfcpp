27f850b0c50a1285d878b57aca724f1bd2856cbb
Dat Dinhquoc <datdinhquoc@abivin.com>
2019-05-19 05:01:31 +0000

    Msg

--- src/tfcpp/model.hpp
+++ src/tfcpp/model.hpp
@@ -7,13 +7,37 @@
 #ifndef TFCPP_MODEL_HPP
 #define TFCPP_MODEL_HPP
 
+//lib headers
+#include <tensorflow/cc/ops/standard_ops.h>         //operations
+#include <tensorflow/cc/framework/gradients.h>      //optimisers
+#include <tensorflow/core/framework/tensor.h>       //data
+#include <tensorflow/core/framework/tensor_shape.h> //data
+#include <tensorflow/cc/client/client_session.h>    //run
+
+//use namespaces (libs)
+using namespace tensorflow;
+using namespace tensorflow::ops;
+
+//shortcuts
+namespace tf = tensorflow;
+
 //namespaces
 namespace tfcpp {
+
+  //external
+  extern Scope R;
 
   /*!
   \brief Generic model class
   */
   class model {
+
+    //properties
+    public:
+      Placeholder Input    = Placeholder(R, DT_FLOAT);
+      Placeholder Expected = Placeholder(R, DT_FLOAT);
+
+    //constructors & methods
     public:
       model();
   };
