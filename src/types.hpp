b2e8de4521073b16ac409bffeeeba3a6ce58dbae
Dat Dinhquoc <datdinhquoc@abivin.com>
2019-05-27 08:23:01 +0000

    Msg

--- src/types.hpp
+++ src/types.hpp
@@ -10,20 +10,20 @@
 //primitives
 /*
 //Systems:                             16bit  32bit  64bit
-typedef unsigned char      byte;     //1byte  1byte  1byte
+typedef unsigned char      byte_;    //1byte  1byte  1byte
 typedef unsigned short     word;     //2bytes 2bytes 2bytes
 typedef unsigned int       dword;    //2bytes 4bytes 4bytes
 typedef unsigned long      qword;    //--     4bytes 8bytes
 typedef unsigned long long oword;    //--     --     8bytes
 
-typedef char               char__;   //the same like unsigned above.
-typedef short              short__;  //the same.
-typedef int                int__;    //the same.
-typedef long               long__;   //the same.
+typedef char               char_;    //the same like unsigned above.
+typedef short              short_;   //the same.
+typedef int                int_;     //the same.
+typedef long               long_;    //the same.
 typedef long long          llong;    //the same.
 
-typedef float              float__;  //4bytes 4bytes 4bytes
-typedef double             double__; //--     8bytes 8bytes
+typedef float              float_;   //4bytes 4bytes 4bytes
+typedef double             double_;  //--     8bytes 8bytes
 typedef long double        ldouble;  //--     --     8bytes
 */
 
@@ -33,20 +33,20 @@ typedef long double        ldouble;  //--     --     8bytes
 //use another word 'bite' for byte instead.
 //long double: https://stackoverflow.com/a/15176438/5581893
 //Systems:                            16bit  32bit  64bit
-#define bite     unsigned char      //1byte  1byte  1byte
+#define byte_    unsigned char      //1byte  1byte  1byte
 #define word     unsigned short     //2bytes 2bytes 2bytes
 #define dword    unsigned int       //2bytes 4bytes 4bytes
 #define qword    unsigned long      //--     4bytes 8bytes
 #define oword    unsigned long long //--     --     8bytes
 
-#define char__   char               //the same like unsigned above.
-#define short__  short              //the same.
-#define int__    int                //the same.
-#define long__   long               //the same.
+#define char_    char               //the same like unsigned above.
+#define short_   short              //the same.
+#define int_     int                //the same.
+#define long_    long               //the same.
 #define llong    long long          //the same.
 
-#define float__  float              //4bytes 4bytes 4bytes
-#define double__ double             //--     8bytes 8bytes
+#define float_   float              //4bytes 4bytes 4bytes
+#define double_  double             //--     8bytes 8bytes
 #define ldouble  long double        //--     --     8bytes
 
 /*!
